d7c1e8ad1a3088aa1bade69cf6352675f6a4cfd5
MorrisCom <57181186+MorrisCom@users.noreply.github.com>
2020-07-11 20:58:51 +0800

    移動

--- 2D_Run/Assets/Scripts/Player.cs
+++ 2D_Run/Assets/Scripts/Player.cs
@@ -12,32 +12,50 @@ public class Player : MonoBehaviour
      */
 
     #region 區域註解
-    [Header("速度"), Range(100, 1000)]
-    public int speed = 100;
+    [Header("速度"), Range(0, 1000)]
+    public int speed = 4;
     [Header("血量")]
     public float hp = 100;
     [Header("金幣數量")]
     public int coin;
-    [Header("跳躍高度")]
-    public int hight = 400;
+    [Header("跳躍高度"), Range(0, 1000)]
+    public int hight = 200;
     [Header("角色動畫")]
     public Animator Ani;
     public AudioClip SJ;
     public AudioClip SS;
     public AudioClip SH;
     public bool dead;
-
+    public CapsuleCollider2D cc2D;
+    public Rigidbody2D rig;
+    public bool isGround;
     #endregion
 
 
     #region 方法
+    private void Move()
+    {
+        if (rig.velocity.magnitude < 10)
+        {
+            rig.AddForce(new Vector2(speed, 0));
+        }
+    }
     /// <summary>
     /// 角色跳躍
     /// </summary>
     private void Jump()
     {
         bool key = Input.GetKey(KeyCode.LeftAlt);
-        Ani.SetBool("跳躍開關", key);
+        Ani.SetBool("跳躍開關", !isGround);
+        if (isGround)
+        {
+            if (key)
+            {
+                rig.AddForce(new Vector2(0, hight));
+                isGround = false;
+            }
+
+        }
     }
     /// <summary>
     /// 角色滑行
@@ -46,6 +64,16 @@ public class Player : MonoBehaviour
     {
         bool key = Input.GetKey(KeyCode.LeftControl);
         Ani.SetBool("滑行開關", key);
+        if (key)
+        {
+            cc2D.offset = new Vector2(-0.3369139f, -0.4f);
+            cc2D.size = new Vector2(4.638223f, 6f);
+        }
+        else
+        {
+            cc2D.offset = new Vector2(-0.3369139f, -0.4360113f);
+            cc2D.size = new Vector2(4.638223f, 6.112195f);
+        }
     }
     /// <summary>
     /// 角色碰撞
@@ -71,16 +99,37 @@ public class Player : MonoBehaviour
     #endregion
     private void Start()
     {
-      
-        
+
+
     }
     private void Update()
     {
-        Jump();
         Slide();
+    }
+    /// <summary>
+    /// 固定更新 一秒50次 有剛體建議用
+    /// </summary>
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "地板")
+        {
+            isGround = true;
+        }
+
+        if (collision.gameObject.name == "懸浮地板")
+        {
+            isGround = true;
+        }
+
     }
 
 
 
- 
+
 }
